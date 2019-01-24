09734dc9431efb61c29db7f612e128e348da93c5
Paul G <thepaulgoldberg@gmail.com>
2019-01-23 18:18:09 -0800

    Start on Deck of Cards

---
+++ 01-javascript/03-js-oop/04-deck-of-cards.js
@@ -0,0 +1,16 @@
+class Card {
+    contructor() {
+        const suits = ["Clubs", "Spades", "Diamonds", "Hearts"]
+        const values = [["Ace", 1], ["Two", 2],  ["Three", 3], ["Four", 4],  ["Five", 5], ["Six", 6], ["Seven", 7], ["Eight", 8], ["Nine", 9] , ["Ten", 10], ["Jack", 11], ["Queen", 12], ["King", 13]]
+
+    }
+    showCard() {
+        console.log("fillllller")
+    }
+}
+
+class Deck extends Card {
+    constructor() {
+        
+    }
+}
